837db35c27dc237a9fdba1270fafa43ab2a82165
bob <bob910078@gmail.com>
2020-07-11 14:59:28 +0800

    66. Plus One

---
+++ LeetcodeTests/Solution Tests/Test Plus One.swift
@@ -0,0 +1,35 @@
+//
+//  Test Plus One.swift
+//  LeetcodeTests
+//
+//  Created by bobchang on 2020/7/11.
+//  Copyright © 2020 Blasphmy. All rights reserved.
+//
+
+import XCTest
+
+class Test_Plus_One: XCTestCase {
+    
+    func testPlusOne1() {
+        let input = [1,2,3]
+        let output = plusOne(input)
+        let ans = [1,2,4]
+        XCTAssertEqual(output, ans)
+    }
+    
+    func testPlusOne2() {
+        let input = [4,3,2,1]
+        let output = plusOne(input)
+        let ans = [4,3,2,2]
+        XCTAssertEqual(output, ans)
+    }
+    
+    func testPlusOne3() {
+        let input = [9]
+        let output = plusOne(input)
+        let ans = [1,0]
+        XCTAssertEqual(output, ans)
+    }
+    
+
+}
---
+++ Solution/Plus One.swift
@@ -0,0 +1,39 @@
+//
+//  Plus One.swift
+//  Leetcode
+//
+//  Created by bobchang on 2020/7/11.
+//  Copyright © 2020 Blasphmy. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 66. Plus One
+ 
+ Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+ The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+ You may assume the integer does not contain any leading zero, except the number 0 itself.
+ */
+
+func plusOne(_ digits: [Int]) -> [Int] {
+    var arr = digits
+    var index = digits.count-1
+    if arr[index] + 1 < 10 {
+        arr[index] += 1
+        return arr
+    }
+    var carry = 1
+    while carry > 0 {
+        if index < 0 {
+            return [carry] + arr
+        }
+        let sum = arr[index] + carry
+        arr[index] = sum % 10
+        carry = sum / 10
+        index -= 1
+    }
+    return arr
+}
