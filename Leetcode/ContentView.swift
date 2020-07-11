6e151429b6b337b10615c4dfed27a44321f82f16
bob <bob910078@gmail.com>
2020-07-11 14:43:11 +0800

    Initial Commit

---
+++ Leetcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leetcode
+//
+//  Created by bobchang on 2020/7/11.
+//  Copyright © 2020 Blasphmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
